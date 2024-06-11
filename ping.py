5537c494a2a5d4b0a3a5d223f98e074d97eb5a22
minhyeok <ga111o@proton.me>
2024-06-11 13:58:36 +0900

    ✨ print time

--- ping.py
+++ ping.py
@@ -5,10 +5,15 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import EMAIL
+import datetime as dt
+dt.datetime.now()
+t = dt.datetime.now()
+print(f"{t.year}-{t.month}-{t.day} {t.hour}:{t.minute}:{t.second}")
+
+printNowTime = f"{t.year}-{t.month}-{t.day} {t.hour}:{t.minute}:{t.second}"
 
 json_file_path = './urls.json'
 
-# 이메일 전송 함수
 def send_email(recipient, url):
     sender_email = EMAIL.sender_email
     password = EMAIL.app_password
@@ -27,11 +32,12 @@ def send_email(recipient, url):
         server.login(sender_email, password)
         server.sendmail(sender_email, recipient, message.as_string())
         server.quit()
-        print(f"메일 전송 성공: {recipient}")
+        
+        print(f"{printNowTime} | 메일 전송 성공: {recipient}")
     except Exception as e:
-        print(f"메일 전송 실패: {e}")
+        
+        print(f"{printNowTime} | 메일 전송 실패: {e}")
 
-# URL 상태를 확인하고 JSON 데이터에 기록하는 함수
 def check_urls(json_data):
     for item in json_data.values():
         if isinstance(item, dict) and 'url' in item and 'mail' in item:
@@ -40,26 +46,27 @@ def check_urls(json_data):
             try:
                 response = requests.get(url, timeout=10)
                 if response.status_code == 200:
-                    print(f"{url} 정상 작동")
+                    
+                    print(f"{printNowTime} | {url} 정상 작동")
                     item['work'] = 1
                 else:
-                    print(f"{url} 작동하지 않음, 메일을 보냅니다.")
+                    
+                    print(f"{printNowTime} | {url} 작동하지 않음, 메일을 보냅니다.")
                     item['work'] = 0
-                    # mail 값이 null이 아닐 때만 메일을 보냄
                     if mail != "NULL":
                         send_email(mail, url)
             except requests.exceptions.RequestException as e:
-                print(f"{url} 연결 실패, 메일을 보냅니다.")
+                
+                print(f"{printNowTime} | {url} 연결 실패, 메일을 보냅니다.")
                 item['work'] = 0
-                # mail 값이 null이 아닐 때만 메일을 보냄
                 if mail != "NULL":
                     send_email(mail, url)
     write_json(json_file_path, json_data)
 
 
-# JSON 파일을 읽는 함수
 def read_json(file_path):
     if not os.path.exists(file_path):
+        
         print(f"파일이 없습니다. {file_path} 파일을 생성합니다.")
         default_data = {
             "http://example.com": {
@@ -72,11 +79,9 @@ def read_json(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
-# JSON 파일을 쓰는 함수
 def write_json(file_path, data):
     with open(file_path, 'w') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
-# 메인 실행 부분
 json_data = read_json(json_file_path)
 check_urls(json_data)
